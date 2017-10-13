42576381b4be4e416507870a783cdd9e22e39e81
Sullivan Ford <sullyjhf@hotmail.co.uk>
2017-10-13 12:28:38 +0100

    Stop bubble sort when done, instead of running to the end of the array

--- sort-visualiser/src/public/js/bubble-sorter.js
+++ sort-visualiser/src/public/js/bubble-sorter.js
@@ -5,6 +5,7 @@ export class BubbleSorter {
     this.array = array;
     this.curr = 0;
     this.length = array.length - 1;
+    this.swappedCount = 0;
     this.done = false;
   }
 
@@ -12,16 +13,19 @@ export class BubbleSorter {
     if (this.done) return;
     if (this.array[this.curr + 1] < this.array[this.curr]) {
       swap(this.array, this.curr + 1, this.curr);
+      this.swappedCount++;
     }
-    this.incCurr();
-  }
 
-  incCurr() {
     this.curr++;
 
     if(this.curr + 1 > this.length) {
       this.length--;
       this.curr = 0;
+      if (this.swappedCount <= 0) {
+        this.done = true;
+      } else {
+        this.swappedCount = 0;
+      }
     }
 
     if (this.length <= 0) {
